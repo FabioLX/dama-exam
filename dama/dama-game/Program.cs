aa3a9bf036fee02ccd980585e10252cee2a9eb38
LX <rarodroid@gmail.com>
2017-11-22 00:43:20 +0100

    stampa elenco mosse giocatori

--- dama/dama-game/Program.cs
+++ dama/dama-game/Program.cs
@@ -5,8 +5,11 @@ namespace dama_game
 {
     class Program
     {
+        List<String> moveStringPlayer1 = new List<String>();
+        List<String> moveStringPlayer2 = new List<String>();
         static void Main(string[] args)
         {
+            Program p = new Program();
             var p1 = new Player("Matteo");
             var p2 = new PlayerMediumComputer("CPU1");
             IScoreObserver observerScore1 = new Player1Scorer();
@@ -15,11 +18,12 @@ namespace dama_game
             //raccoglitore mosse
             List<Chessboard> moveListPlayer1 = new List<Chessboard>();
             List<Chessboard> moveListPlayer2 = new List<Chessboard>();
+          
 
             //int scorePlayer1 = 0;
             //int scorePlayer2 = 0;
 
-            
+
             IPlayer toMove = p1;
             tableScore.RegisterObserver(observerScore1);
             tableScore.RegisterObserver(observerScore2);
@@ -45,7 +49,7 @@ namespace dama_game
                     char s = Console.ReadKey().KeyChar;
                     xs = b.PositionConverter(s);//addon
 
-                    Console.WriteLine();
+                Console.WriteLine();
                 Console.Write("H:");
                     ys = (int)Char.GetNumericValue(Console.ReadKey().KeyChar);
                     Console.WriteLine();
@@ -68,14 +72,22 @@ namespace dama_game
                     if (toMove == p1)
                     {
                         moveListPlayer1.Add(b);//salvo mosse
+                        p.moveStringPlayer1.Add("From: "+s.ToString()+" "+ys.ToString()+" To: "+s2.ToString()+" "+ye.ToString());
                         toMove = p2;
+                       
                     }
                     else
                     {
                         moveListPlayer2.Add(b);//salvo mosse
+                        p.moveStringPlayer2.Add("From: " + s.ToString() + " " + ys.ToString() + " To: " + s2.ToString() + " " + ye.ToString());
                         toMove = p1;
+                        
                     }
                     //<--------ESITO MOSSA OK
+                    //mock print mossa
+                    //Console.WriteLine("From: " + s.ToString() + " " + ys.ToString() + "To: " + s2.ToString() + " " + ye.ToString());
+                    //p.PrintFinalScore(); //STAMPA ELENCO MOSSE
+                    //Console.ReadLine();
                 }
                 catch(Exception e){
                     Console.WriteLine(e.Message);
@@ -84,7 +96,11 @@ namespace dama_game
                 }
 
             } while (b.Winner == null);
+            //stampa elenco mosse
+            p.PrintFinalScore();
 
+
+            //simulazione a fine partita  - STAND BY!
             Console.WriteLine("Press to simulate legal move (5,1) -> (4,2)...");
             Console.ReadLine();
 
@@ -97,8 +113,20 @@ namespace dama_game
 
             b.SimulatePlay();
 
-            Console.WriteLine("Winner is {0} with score {1}", b.Winner.Name, b.Winner.Score);
+            Console.WriteLine("Winner is {0} with score {1}", b.Winner.Name, b.Winner.Score);            
             Console.WriteLine("Loser is {0} with score {1}", b.Loser.Name, b.Loser.Score);
+            
+        }
+
+        private void PrintFinalScore()
+        {
+            //Stampa brutale delle mosse!
+            Console.WriteLine("Player1 Moves");
+            Console.WriteLine(String.Join("\n", moveStringPlayer1));
+            Console.WriteLine("Player2 Moves");
+            Console.WriteLine(String.Join("\n", moveStringPlayer2));
         }
     }
+
+   
 }
